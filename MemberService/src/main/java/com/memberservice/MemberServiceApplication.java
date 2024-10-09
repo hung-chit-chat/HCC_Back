d01b030bf1bc73defdfe4cc414d8e8ede1574495
yudong-yeob <maintain98@naver.com>
2024-10-09 22:36:03 +0900

    fix : [ #none docker ]

--- MemberService/src/main/java/com/memberservice/MemberServiceApplication.java
+++ MemberService/src/main/java/com/memberservice/MemberServiceApplication.java
@@ -1,11 +1,17 @@
 package com.memberservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,                // 시큐리티 자동 구성 끄기, passwordEncoder 만 사용
+        ManagementWebSecurityAutoConfiguration.class    // Actuator 의 보안 설정을 제외
+})
 public class MemberServiceApplication {
 
     public static void main(String[] args) {
