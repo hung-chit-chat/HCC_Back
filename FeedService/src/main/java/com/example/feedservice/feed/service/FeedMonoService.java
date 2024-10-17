7ce2104c1ae188793697953579374e04b4c7cda3
yeob <maintain98@naver.com>
2024-10-17 17:11:23 +0900

    refactor : [ FeedMonoService 코드 수정 및 간소화]

--- FeedService/src/main/java/com/example/feedservice/feed/service/FeedMonoService.java
+++ FeedService/src/main/java/com/example/feedservice/feed/service/FeedMonoService.java
@@ -19,6 +19,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -36,6 +37,8 @@ public class FeedMonoService {
     private final RedisTemplate<String, ResponseFeedDto> redisTemplate;
 
     private final FeedRestService feedRestService;
+
+    private static final int BATCH_SIZE = 5;
 
     public FeedMonoService(@Qualifier("feedListRedisTemplate") RedisTemplate<String, ResponseFeedDto> redisTemplate, FeedRestService feedRestService) {
         this.redisTemplate = redisTemplate;
@@ -78,78 +81,81 @@ public class FeedMonoService {
      * */
     protected ResponseFeedDto saveRedisAndReturnRemainingFeed(LocalDateTime cursorDate, List<FeedListDto> feedList){
         // 5개를 제외한 데이터 저장
-        List<FeedListDto> feedSkipList = feedList.stream().skip(5).toList();
+        List<FeedListDto> feedSkipList = feedList.size() > 5 ? feedList.subList(5, feedList.size()) : Collections.emptyList();
 
-        LocalDateTime returnNextCursorDate = null;
-
-        long size = feedList.stream().limit(5).toList().size();
+        // feedList 가 5개보다 많으면 반환하고 남은 데이터중 첫번째 createDate 를 저장
+        LocalDateTime returnNextCursorDate = !feedSkipList.isEmpty() ? this.getFirstCreatedDate(feedSkipList) : null;
 
         // 데이터가 남았는지 확인
-        if(!feedSkipList.isEmpty()){
-            returnNextCursorDate = feedList.get(feedList.stream().limit(5).toList().size()).getCreatedDate();
-            int batchSize = 5;
+        if(returnNextCursorDate != null){
             int remainingSize = feedSkipList.size();        // 반환하고 남은 데이터
-            int loopCount = (int) Math.ceil((double) remainingSize / batchSize);        // 나눠야 할 배치 수 계산 1~2    0이면 5개 이하
+            int loopCount = (int) Math.ceil((double) remainingSize / BATCH_SIZE);        // 나눠야 할 배치 수 계산 1~2    0이면 5개 이하
 
             for (int i = 0; i < loopCount; i++) {
-                boolean hasMore = true;
+                boolean hasMore = (i + 1 < loopCount);
 
                 // 현재 배치에서 5개씩 데이터 가져옴
                 List<FeedListDto> feedBatch = feedList.stream()
-                        .skip((long) (i + 1) * batchSize)
-                        .limit(batchSize)
+                        .skip((long) (i + 1) * BATCH_SIZE)
+                        .limit(BATCH_SIZE)
                         .toList();
 
                 LocalDateTime nextCursorDate = null;
 
                 // 다음 배치의 첫 번째 항목을 nextCursorDate 로 설정
-                if(i + 1 < loopCount){
+                if(hasMore){
                     List<FeedListDto> nextFeedBatch = feedSkipList.stream()
-                            .skip((long) (i + 1) * batchSize)  // 다음 배치로 넘어감
+                            .skip((long) (i + 1) * BATCH_SIZE)  // 다음 배치로 넘어감
                             .limit(1)  // 첫 번째 항목만 가져옴
                             .toList();
                     if(!nextFeedBatch.isEmpty()){
-                        nextCursorDate = nextFeedBatch.get(0).getCreatedDate();
+                        nextCursorDate = this.getFirstCreatedDate(nextFeedBatch);
                     }
                 }
 
-                // 마지막 루프면 hasMore = false, nextCursorDate는 null
-                if (i == loopCount - 1) {
-                    hasMore = false;
-                    nextCursorDate = null;
-                }
-
-                ResponseFeedDto batchResponse = ResponseFeedDto.builder()
-                            .feedListDto(feedBatch)
-                            .cursorDate(nextCursorDate != null ? nextCursorDate.toString() : feedBatch.get(0).getCreatedDate().toString())
-                            .nextCursorDate(nextCursorDate.toString())
-                            .hasMore(hasMore)
-                            .build();
-
-
-                // 각 배치를 저장할 때, 새로운 커서를 키로 사용하여 Redis에 저장
-                String redisKey = feedBatch.get(0).getCreatedDate().toString();
-                redisTemplate.opsForValue().set(redisKey,
-                        batchResponse, 600, TimeUnit.SECONDS);
+                this.saveRedis(feedBatch, nextCursorDate, hasMore);
             }
 
             // 첫 데이터 5개를 반환
             return ResponseFeedDto.builder()
                     .cursorDate(cursorDate.toString())
                     .feedListDto(feedList.stream().limit(5).toList())
-                    .nextCursorDate(returnNextCursorDate != null ? returnNextCursorDate.toString() : feedList.get(feedList.size() - 1).getCreatedDate().toString())
+                    .nextCursorDate(returnNextCursorDate.toString())
                     .hasMore(true)
                     .build();
 
         } else{
-
             // 데이터가 5개 이하인 경우 모두 반환
             return ResponseFeedDto.builder()
                     .cursorDate(cursorDate.toString())
-                    .nextCursorDate(null)
                     .feedListDto(feedList)
+                    .nextCursorDate(null)
                     .hasMore(false)
                     .build();
         }
+    }
+
+    /**
+     * 레디스에 저장
+     * */
+    protected void saveRedis(List<FeedListDto> feedBatch, LocalDateTime nextCursorDate, boolean hasMore){
+        String redisKey = this.getFirstCreatedDate(feedBatch).toString();
+
+        ResponseFeedDto batchResponse = ResponseFeedDto.builder()
+                .feedListDto(feedBatch)
+                .cursorDate(nextCursorDate != null ? nextCursorDate.toString() : this.getFirstCreatedDate(feedBatch).toString())
+                .nextCursorDate(nextCursorDate != null ? nextCursorDate.toString() : null)
+                .hasMore(hasMore)
+                .build();
+
+        redisTemplate.opsForValue().set(redisKey,
+                batchResponse, 600, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 리스트 중 첫번째 createdDate 반환
+     * */
+    protected LocalDateTime getFirstCreatedDate(List<FeedListDto> feedBatch){
+        return feedBatch.get(0).getCreatedDate();
     }
 }
