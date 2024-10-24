0e545d93b9bd42cd4b9a1a99bf2d6f5756d8b19b
yudong-yeob <maintain98@naver.com>
2024-10-24 21:03:41 +0900

    none : [ 커밋 ]

--- MemberService/src/test/java/com/memberservice/config/MockAppConfig.java
+++ MemberService/src/test/java/com/memberservice/config/MockAppConfig.java
@@ -1,38 +1,38 @@
-package com.memberservice.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-public class MockAppConfig {
-
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    public static ObjectMapper objectMapper() {
-        Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder = new Jackson2ObjectMapperBuilder();
-        return jackson2ObjectMapperBuilder.build();
-    }
-
-    public static RedisTemplate<String, Object> redisTemplate() {
-        RedisConnectionFactory redisConnectionFactory = new LettuceConnectionFactory();
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
-    }
-
-
-
-}
+//package com.memberservice.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//public class MockAppConfig {
+//
+//    public static PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    public static ObjectMapper objectMapper() {
+//        Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder = new Jackson2ObjectMapperBuilder();
+//        return jackson2ObjectMapperBuilder.build();
+//    }
+//
+//    public static RedisTemplate<String, Object> redisTemplate() {
+//        RedisConnectionFactory redisConnectionFactory = new LettuceConnectionFactory();
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory);
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setHashKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        template.afterPropertiesSet();
+//        return template;
+//    }
+//
+//
+//
+//}
